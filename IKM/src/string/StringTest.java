6f74954c044bcac8b86a5c4a5c128cfe40258b9c
starwavelin <benjaminxianlin@gmail.com>
2018-01-26 16:21:03 -0500

    revisited StringTest

--- IKM/src/string/StringTest.java
+++ IKM/src/string/StringTest.java
@@ -3,8 +3,8 @@ package string;
 public class StringTest {
 
 	public static void main(String[] args) {
-		String s1 = new String("Hello");
-		String s2 = "Hello";
+		String s1 = new String("Hello");	// from what ?
+		String s2 = "Hello";				// from string pool ?
 		
 		System.out.println(s1 == s2); // FALSE!!! because two different char arrays?
 		System.out.println(s1.equals(s2)); // true
@@ -12,5 +12,14 @@ public class StringTest {
 		
 		System.out.println(s2 == "Hello"); // true
 		System.out.println(s1 == "Hello"); // false
+		
+		
+		/* String + int, what's gonna happen? */
+		String s = "Hey";
+		int k = 12;
+		String t = s + k;
+		String nt = k + s;
+		System.out.println("t is: " + t); 	// "Hey12"
+		System.out.println("nt is: " + nt);	// "12Hey"
 	}
 }
