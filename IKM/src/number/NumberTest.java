36b6559b13ba8f28267e0def403d961e13d925b0
starwavelin <benjaminxianlin@gmail.com>
2018-03-01 09:52:47 -0500

    test two divisions in NumberTest

--- IKM/src/number/NumberTest.java
+++ IKM/src/number/NumberTest.java
@@ -5,10 +5,10 @@ public class NumberTest {
 	public static void main(String[] args) {
 		double d = 2.0;
 		System.out.println(2.0 == 2); // TRUE!!!
-		
-		
-		
 		float f = 1.9f;
+		
+		System.out.println(-1 / 2); // 0
+		System.out.println(-3 / 2); // -1
 	}
 	
 }
